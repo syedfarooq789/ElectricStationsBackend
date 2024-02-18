2e89723ab5f5fd449273db78077d1667282fe4f1
Farooq Hassan <farooqhassan92@hotmail.com>
2024-02-18 17:27:14 +0200

    fixing database path in company module

--- src/modules/company/company.module.ts
+++ src/modules/company/company.module.ts
@@ -3,7 +3,7 @@ import { Module } from "@nestjs/common";
 import { CommonModule } from "../common";
 import { CompanyController } from "./controller";
 import { CompanyService } from "./service";
-import { DatabaseModule } from "../database/database.module";
+import { DatabaseModule } from "../database";
 import { companyProvider } from "../company/provider";
 import { StationModule } from "../station/station.module";
 @Module({
